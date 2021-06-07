6dd8dc122271959d9a0b964bd690a7eca0855825
joandy alejo garcia <63996685+Andy-Not@users.noreply.github.com>
2021-06-06 20:38:48 -0400

    useing Refs instead of states

--- my-app/src/componets/UserInput/TaskInfo.js
+++ my-app/src/componets/UserInput/TaskInfo.js
@@ -1,53 +1,48 @@
-import React, {useState} from "react";
+import React, {useRef} from "react";
 
 const TaskInfo = (props) => {
 
-   const [time,setTime] = useState();
-   const [date, setDate] = useState();
-   const [title, setTitle] = useState("");
+    const timeInputRef = useRef();
+    const dateInputRef = useRef();
+    const titleInputRef = useRef();
 
    const submitHandler = (event) => {
-       event.preventDefault();
+
+       event.preventDefault(event);
+
+       const date = new Date(dateInputRef.current.value)
+
+       let timeSelected = timeInputRef.current.value.substring(0,2)
+
+       let time = null;
+
+       if (+timeSelected >= 12){
+           time = `${timeInputRef.current.value} PM`;
+       } else if (+timeSelected) {
+           time = `${timeInputRef.current.value} AM`;
+       }
+
          const newTask = {
-             title: title,
-             due: date,
+             title: titleInputRef.current.value,
+             due: `${date.getMonth()+1}/${date.getDate()+1}/${date.getFullYear()}`,
              time: time,
              id: Math.random()
          }
+
          props.addTask(newTask)
-         setTitle("")
-   }
+       titleInputRef.current.value = ""
 
-   const timeHandler = (event) => {
-
-       let timeSelected = event.target.value.substring(0,2)
-
-       if (+timeSelected >= 12){
-           setTime(event.target.value + " PM")
-       } else {
-           setTime(event.target.value + " AM")
-       }
-
-   }
-
-   const dateHandler = (event) => {
-       const xdate = new Date(event.target.value)
-        setDate(`${xdate.getMonth()+1}/${xdate.getDate()+1}/${xdate.getFullYear()}`)
-   }
-
-   const titleHandler = (event) => {
-        setTitle(event.target.value);
    }
 
     return(
         <form onSubmit={submitHandler}>
             <div className="form_task_title">
                 <label htmlFor="task">Task</label>
-                <input onChange={titleHandler} id={"task"} type="text" value={title}/>
+                <input id={"task"} type="text" ref={titleInputRef}/>
             </div>
             <label htmlFor="date"> date and time</label>
-            <input onSelect={dateHandler} type="date"/>
-            <input onSelect={timeHandler} type="time" name="time" placeholder="hrs:mins" pattern="^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$" class="inputs time"/>
+            <input  type="date" ref={dateInputRef}/>
+            <input  type="time" name="time" placeholder="hrs:mins" pattern="^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$" class="inputs time" ref={timeInputRef}/>
             <button type={"submit"}>Add task</button>
         </form>
     );
